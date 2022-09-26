9334d8104dfd341f8c0f753328049a5d5d20c073
JakeBareng <barengjake@gmail.com>
2022-09-25 22:00:28 -0700

    added: css styling more html tags for organization

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const searchBar = document.querySelector('input')
-const temperature = document.querySelector('.temperature')
-const feelsLike = document.querySelector('.feels-like')
-const humidity = document.querySelector('.humidity')
-const wind = document.querySelector('.wind')
-const country = document.querySelector('.country')
-const city = document.querySelector('.city')
-const description = document.querySelector('.description')
+const temperature = document.querySelector('.temperature > p')
+const feelsLike = document.querySelector('.feels-like > p')
+const humidity = document.querySelector('.humidity > p')
+const wind = document.querySelector('.wind > p')
+const country = document.querySelector('.country > h4')
+const city = document.querySelector('.city > h3')
+const description = document.querySelector('.description > p')
 
 
 async function weatherAPI(city) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=08703343fa371f1d401a54461da661b7`
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=08703343fa371f1d401a54461da661b7`
     let response = await fetch(api, {mode: 'cors'});
     let data = await response.json();
     let processedData = processJSON(data);
@@ -17,7 +17,7 @@ async function weatherAPI(city) {
 }
 
 function processJSON(data) {
-    let temperature = data.main.temp
+    let temperature = data.main.temp;
     let feelsLike = data.main.feels_like;
     let humidity = data.main.humidity;
     let countryName = new Intl.DisplayNames(['en'], {type: 'region'}).of(data.sys.country);
@@ -27,17 +27,25 @@ function processJSON(data) {
     return {temperature,feelsLike,humidity,countryName,city,wind,description}
 }
 
+function clearDisplay() {
+    city.innerText = ""
+    country.innerText = ""
+    description.innerText = ""
+    feelsLike.innerText = ""
+    humidity.innerText =""
+    temperature.innerText = ""
+    wind.innerText = ""
+}
+
 function display(data) {
-    console.log(data);
     city.innerText = data.city
     country.innerText = data.countryName
-    description.innerText = data.description
+    description.innerText = '"' + data.description + '"'
     feelsLike.innerText = data.feelsLike
-    humidity.innerText = data.humidity
+    humidity.innerText = data.humidity +"%"
     temperature.innerText = data.temperature
     wind.innerText = data.wind
 }
-
 
 function eventHandlerSearch() {
     let city = searchBar.value
